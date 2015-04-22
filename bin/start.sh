50d9859e8d680042bc9ca820712099fc0ca6911d
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2015-04-22 12:22:56 -0400

    Changed docker MONGO variable

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,5 @@
 # Configure MongoDB
-sed s/localhost:27017/$MONGODB_PORT_28017_TCP_ADDR:$MONGODB_PORT_27017_TCP_PORT/g /etc/agate/application.yml > /tmp/application.yml
+sed s/localhost:27017/$MONGO_PORT_28017_TCP_ADDR:$MONGO_PORT_27017_TCP_PORT/g /etc/agate/application.yml > /tmp/application.yml
 mv -f /tmp/application.yml /etc/agate/application.yml
 chown -R agate:adm /etc/agate
 
