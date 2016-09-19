1da2686f458ab722df616699158996958b9e58c8
Yannick Marcon <yannick.marcon@obiba.org>
2016-09-19 11:28:29 +0200

    1.2-snapshot tag added

---
+++ 1.2-snapshot/bin/first_run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Configure administrator password
+adminpw=$(echo -n $AGATE_ADMINISTRATOR_PASSWORD | xargs java -jar /usr/share/agate/tools/lib/obiba-password-hasher-*-cli.jar)
+cat $AGATE_HOME/conf/shiro.ini | sed -e "s,^administrator\s*=.*\,,administrator=$adminpw\,," > /tmp/shiro.ini && \
+    mv /tmp/shiro.ini $AGATE_HOME/conf/shiro.ini
+
+# Configure MongoDB
+if [ -n "$MONGO_PORT_27017_TCP_ADDR" ]
+	then
+	sed s/localhost:27017/$MONGO_PORT_27017_TCP_ADDR:$MONGO_PORT_27017_TCP_PORT/g $AGATE_HOME/conf/application.yml > /tmp/application.yml
+	mv -f /tmp/application.yml $AGATE_HOME/conf/application.yml
+fi
+
+# Configure ReCaptcha
+if [ -n "$RECAPTCHA_SITE_KEY" -a -n "$RECAPTCHA_SECRET_KEY" ]
+	then
+	sed s/secret:\ 6Lfo7gYT.*/secret:\ $RECAPTCHA_SECRET_KEY/ $AGATE_HOME/conf/application.yml | \
+	sed s/reCaptchaKey:.*/reCaptchaKey:\ $RECAPTCHA_SITE_KEY/ > /tmp/application.yml
+	mv -f /tmp/application.yml $AGATE_HOME/conf/application.yml
+fi
--- Makefile
+++ Makefile
@@ -7,3 +7,6 @@ no_cache=false
 # Build Docker image
 build:
 	sudo docker build --no-cache=$(no_cache) -t="obiba/agate:snapshot" .
+
+build12x:
+	sudo docker build --no-cache=$(no_cache) -t="obiba/agate:1.2-snapshot" 1.2-snapshot 
