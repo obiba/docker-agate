4fbdd00810ce0d919107912d54f7c94f075897c9
Yannick Marcon <yannick.marcon@obiba.org>
2024-03-16 20:26:01 +0100

    migrate application.yml in start script

--- bin/start.sh
+++ bin/start.sh
@@ -13,18 +13,28 @@ fi
 # Make sure conf folder is available
 if [ ! -d $AGATE_HOME/conf ]
 	then
-	mkdir -p $AGATE_HOME/conf
-	cp -r /usr/share/agate/conf/* $AGATE_HOME/conf
-	# So that application is accessible from outside of docker
-	sed s/address:\ localhost//g $AGATE_HOME/conf/application.yml > /tmp/application.yml && \
+		mkdir -p $AGATE_HOME/conf
+		cp -r /usr/share/agate/conf/* $AGATE_HOME/conf
+		# So that application is accessible from outside of docker
+		sed s/address:\ localhost//g $AGATE_HOME/conf/application.yml > /tmp/application.yml && \
 	    mv /tmp/application.yml $AGATE_HOME/conf/application.yml
 fi
 
 # Check if 1st run. Then configure application.
 if [ -e /opt/agate/bin/first_run.sh ]
-    then
-    /opt/agate/bin/first_run.sh
-    mv /opt/agate/bin/first_run.sh /opt/agate/bin/first_run.sh.done
+  then
+  	/opt/agate/bin/first_run.sh
+  	mv /opt/agate/bin/first_run.sh /opt/agate/bin/first_run.sh.done
+fi
+
+# Upgrade to 3.x
+if [ -e $AGATE_HOME/conf/application.yml ]
+	then
+		if grep -q "profiles:" $AGATE_HOME/conf/application.yml
+			then
+				cp $AGATE_HOME/conf/application.yml $AGATE_HOME/conf/application.yml.2.x
+				cat $AGATE_HOME/conf/application.yml.2.x | grep -v "profiles:" > $AGATE_HOME/conf/application.yml
+		fi
 fi
 
 # Wait for MongoDB to be ready
